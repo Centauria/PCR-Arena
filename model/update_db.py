637190460c5bd0b9fc9f5149c295016c2bdc6c61
Centauria <centauriavc@gmail.com>
2020-08-10 02:05:45 +0800

    use logging

--- model/update_db.py
+++ model/update_db.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
+import logging
+
 import brotli
 import requests
 
+logger = logging.getLogger(__name__)
 
-def update_db():
+
+def update_db(db_name: str):
     url = 'https://redive.estertion.win/db/redive_cn.db.br'
+    logger.info(f'Getting Princess Connect Re:Dive CN Database at {url}')
     r = requests.get(url)
-    with open('redive_cn.db', 'wb') as f:
+    if r.status_code != requests.codes.ok:
+        logger.warning('Connection problem')
+    with open(db_name, 'wb') as f:
         f.write(brotli.decompress(r.content))
 
 
 if __name__ == '__main__':
-    update_db()
+    update_db('redive_cn.db')
